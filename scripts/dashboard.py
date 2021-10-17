0872c4118076ef6764e30749efb3d44a094ad2b4
Tobias Kirschstein <tobias.kirschstein@gmail.com>
2021-10-17 02:01:23 +0200

    Add Clustering, Multi-Select, real ratings

--- scripts/dashboard.py
+++ scripts/dashboard.py
@@ -2,24 +2,63 @@ from typing import List
 
 import altair as alt
 import hydralit_components as hc
-import numpy as np
 import pandas as pd
 import streamlit as st
 from altair import Scale
 
 from greenspectors.env import COMPANY_NAMES, LABELED_COMPANY_SUSTAINABILITY_PATH, LABELED_KEYWORDS_PATH, KEYWORDS
 
-
 # =========================================================================
 # Data Fetchers
 # =========================================================================
 
-def get_company_rankings() -> List[float]:
-    return list(np.random.rand(len(COMPANY_NAMES)))
+COMPANY_TO_CSV = {
+    'Bank of America': 'BAC',
+    'Exxon Mobile': 'Exxon',
+    'General Motors': 'GM',
+    'Johnson Johnson': 'J&J',
+    'JP Morgan': 'JPMorgan',
+    'Mc Donalds': 'McD',
+    'TD Bank': 'TDBank',
+    'JetBlue': 'Jetblue'
+}
+
+CSV_TO_COMPANY = {
+    'BAC': 'Bank of America',
+    'Exxon': 'Exxon Mobile',
+    'GM': 'General Motors',
+    'J&J': 'Johnson Johnson',
+    'JPMorgan': 'JP Morgan',
+    'McD': 'Mc Donalds',
+    'TDBank': 'TD Bank',
+    'Jetblue': 'JetBlue'
+}
 
 
-def get_company_perceptions() -> List[float]:
-    return list(np.random.rand(len(COMPANY_NAMES)))
+def get_company_rankings(company_names: List[str] = COMPANY_NAMES) -> List[float]:
+    company_ratings = get_company_sustainability_ratings()
+    company_rankings = []
+
+    for company_name in company_names:
+        if company_name in company_ratings['Company name'].values:
+            df_company_name = company_name
+        else:
+            df_company_name = COMPANY_TO_CSV[company_name]
+        ranking = company_ratings.iloc[(company_ratings['Company name'].values == df_company_name).argmax()][
+            'Final Rating']
+        company_rankings.append(ranking / 5)
+
+    return company_rankings
+    # return list(np.random.rand(len(COMPANY_NAMES)))
+
+
+def get_company_perceptions(company_names: List[str] = COMPANY_NAMES) -> List[float]:
+    return [get_company_sustainability_tweets(company_name)['Label'].mean() / 2 for company_name in company_names]
+
+
+@st.cache
+def get_company_sustainability_ratings() -> pd.DataFrame:
+    return pd.read_csv(f"rating/Numeric Rating - Sheet1.csv", skiprows=1)
 
 
 @st.cache
@@ -49,6 +88,11 @@ def get_combined_tweets() -> pd.DataFrame:
     return pd.concat(dataframes)
 
 
+@st.cache
+def get_company_pca_clustering() -> pd.DataFrame:
+    return pd.read_csv("rating/clustering_pca_results.csv")
+
+
 # =========================================================================
 # Pages
 # =========================================================================
@@ -58,15 +102,19 @@ def build_dashboard_page():
     # Ranking of the companies
     st.header("Company Greenliness vs Public Perception")
 
-    # TODO: Add multi-select
     # TODO: Make everything GREEN
 
-    company_rankings = get_company_rankings()
-    company_perceptions = get_company_perceptions()
+    chosen_companies = st.multiselect(
+        'Companies',
+        COMPANY_NAMES,
+        COMPANY_NAMES)
+
+    company_rankings = get_company_rankings(company_names=chosen_companies)
+    company_perceptions = get_company_perceptions(company_names=chosen_companies)
 
     company_rankings_and_perceptions_df = pd.DataFrame([
         {'Company': company, 'Ranking': ranking, 'Perception': perception, 'Discrepancy': abs(ranking - perception)}
-        for company, ranking, perception in zip(COMPANY_NAMES, company_rankings, company_perceptions)
+        for company, ranking, perception in zip(chosen_companies, company_rankings, company_perceptions)
     ])
     company_rankings_and_perceptions_df.set_index('Company')
 
@@ -82,6 +130,7 @@ def build_dashboard_page():
     col1, col2 = st.columns(2)
 
     with col1:
+        st.header("Greenliness")
         st.altair_chart(
             alt.Chart(company_rankings_and_perceptions_df).mark_bar().encode(
                 x='Ranking',
@@ -90,12 +139,48 @@ def build_dashboard_page():
             ).properties(height=700))
 
     with col2:
+        st.header("Public Perception")
         st.altair_chart(
             alt.Chart(company_rankings_and_perceptions_df).mark_bar().encode(
                 x='Perception',
                 color='Perception',
                 y=alt.Y("Company", sort='-x')
             ).properties(height=700))
+
+    # Company Clustering
+    st.header("Company Clustering")
+    company_pca_clustering = get_company_pca_clustering().copy()
+    company_pca_clustering['Public Perception'] = ''
+    company_pca_clustering['Ranking'] = ''
+    company_pca_clustering['Selected'] = False
+    for idx, row in company_pca_clustering.iterrows():
+        company_name = row['Company name']
+        if company_name in CSV_TO_COMPANY:
+            company_name = CSV_TO_COMPANY[company_name]
+
+        is_selected = company_name in chosen_companies
+        company_pca_clustering.loc[idx, 'Selected'] = is_selected
+        if is_selected:
+            company_idx = chosen_companies.index(company_name)
+            company_perception = company_perceptions[company_idx]
+            company_ranking = company_rankings[company_idx]
+
+            company_pca_clustering.loc[idx, 'Public Perception'] = company_perception
+            company_pca_clustering.loc[idx, 'Ranking'] = company_ranking
+
+    company_pca_clustering = company_pca_clustering[company_pca_clustering['Selected'] == True]
+    print(company_pca_clustering)
+
+    st.altair_chart(
+        alt.Chart(company_pca_clustering).mark_point().encode(
+            x='0',
+            y='1',
+            color='Ranking',
+            shape='kmeans:O',
+            tooltip=['Company name', 'Ranking']
+        ),
+        use_container_width=True
+    )
 
 
 def build_company_insights_page():
@@ -105,7 +190,19 @@ def build_company_insights_page():
 
     company_sustainability_tweets = get_company_sustainability_tweets(selected_company)
 
+    company_ranking = get_company_rankings([selected_company])[0]
+    company_perception = company_sustainability_tweets['Label'].mean() / 2
+
     st.header(f"Public Sustainability Opinion of {selected_company}")
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.metric("Sustainability Ranking", company_ranking)
+
+    with col2:
+        st.metric("Public Perception", f"{company_perception: 0.2f}")
+
+    st.header("Historical development of Public Perception")
 
     perception_by_time = company_sustainability_tweets[['Label']]
     perception_by_time['Label'] /= 2
